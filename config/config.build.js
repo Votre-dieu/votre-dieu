d32a5699a77827c7f01f2ef149892c8aaf20150f
crazymryan <2407488005@qq.com>
2021-04-02 23:54:53 +0800

    chore: Components introduce configuration on demand

--- config/config.build.js
+++ config/config.build.js
@@ -1,22 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 const join = path.join;
-//  获取基于当前路径的目标文件
 const resolve = dir => path.join(__dirname, '../', dir);
 
 function getComponentEntries(path) {
     let files = fs.readdirSync(resolve(path));
     const componentEntries = files.reduce((fileObj, item) => {
-        //  文件路径
         const itemPath = join(path, item);
-        //  在文件夹中
         const isDir = fs.statSync(itemPath).isDirectory();
         const [name, suffix] = item.split('.');
-        //  文件中的入口文件
         if (isDir) {
             fileObj[item] = resolve(join(itemPath, 'index.js'));
         }
-        //  文件夹外的入口文件
         else if (suffix === 'js') {
             fileObj[name] = resolve(`${itemPath}`);
         }
@@ -25,24 +20,16 @@ function getComponentEntries(path) {
 
     return componentEntries;
 }
-
 const buildConfig = {
-    //  输出文件目录
     outputDir: resolve('lib'),
-    //  webpack配置
+    productionSourceMap: false,
     configureWebpack: {
-        //  入口文件
         entry: getComponentEntries('packages'),
-        //  输出配置
         output: {
-            //  文件名称
             filename: '[name]/index.js',
-            //  构建依赖类型
-            libraryTarget: 'commonjs2',
-            //  库中被导出的项
             libraryExport: 'default',
-            //  引用时的依赖名
-            library: 'votre-dieu'
+            library: 'votre-dieu',
+            libraryTarget: 'umd'
         }
     },
     css: {
